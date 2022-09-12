b94ca8a0e2c0226911a75929b20f8844af5d9e83
James Lotery <jlotery@gmail.com>
2022-09-12 11:36:30 +0200

    eslint

--- release.config.js
+++ release.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   plugins: [
     '@semantic-release/commit-analyzer',
+    {
+      preset: 'eslint'
+    },
     '@semantic-release/release-notes-generator',
     [
       '@semantic-release/changelog',
